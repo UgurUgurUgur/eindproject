a469ac5f34c44a8e904a3842fcc88c71470b30d0
liamvdm <L1am20041711@gmail.com>
2023-12-23 20:13:45 +0100

    formvalidation added and terms but not IMPLEMENTED

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,29 +14,39 @@ export class RegisterComponent {
   username: any;
   password: any;
   email: any;
+  /* terms: true | false = false; */
+  /* TERMS IS IN COMMENTS BECAUSE IT IS NOT IMPLEMENTED */
 
   onSubmit() {
-    console.log("name: " + this.name);
-    console.log("surname:"  + this.surname);
-    console.log("username: " + this.username);
-    console.log( "email: " + this.email);
-    // We want to keep the password private so we don't send it to the console
 
-
-    // Reset the form after submission so it doesnt get submitted again
-    this.name = '';
-    this.surname = '';
-    this.username = '';
-    this.password = ''; // here we clear the password from the page but do not remove it
-    this.email = '';
+    if (!this.name || !this.surname || !this.username || !this.password || !this.email ) { /* if any of the fields are empty show an alert */
+        alert("Please fill out all fields");
+        return;
+    }
+    /* else if (!this.terms) {
+        alert("Please accept the terms and conditions");
+        return;
+    } */
+    else {
+      console.log("name: " + this.name);
+      console.log("surname:"  + this.surname);
+      console.log("username: " + this.username);
+      console.log( "email: " + this.email);
+      // We want to keep the password private so we don't send it to the console  
+      // Reset the form after submission so it doesnt get submitted again
+      this.name = '';
+      this.surname = '';
+      this.username = '';
+      this.password = ''; // here we clear the password from the page, but do not remove it
+      this.email = '';
+    }
   }
 
-  // TODO: Add form validation
   // TODO: Add error handling
   // TODO: Add email validation
   // TODO: Add password validation
   // TODO: make the route for the login page
-  // TODO: make a term and conditions page with a checkbox that isnt checked by default and needs to be checked by the user to submit
+  // TODO: IMPLEMENT terms and conditions
 }
 
 
