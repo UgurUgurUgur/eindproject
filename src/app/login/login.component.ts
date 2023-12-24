c7d02b059f67ced7be24ae9d47cbd81f126e43f1
liamvdm <L1am20041711@gmail.com>
2023-12-24 15:09:35 +0100

    made the basic for the login page and added todos, added the fontawesome link in the main html and cleaned up the html from register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username: any;
+  password: any;
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword; /* went from true to false */
+  }
+
+
+  /* check(){
+    if (this.username == " " && this.password == ""){         only wil work if password will be stored in a database
+      return true;
+    }
+    else{ 
+      return false;
+    }
+  } */
+
+
+  onSubmit() {
+    if (!this.username || !this.password) { /* if any of the fields are empty show an alert */
+      alert("Please fill out all fields");
+      return;
+    }
+    else {
+      alert("Login successful");
+      console.log("It works");
+    }
+  }
+
+  //TODO: check if password is correct with own database and api
+  //TODO: check if username is correct with own database and api
+  //TODO: Make an admin login
+  //TODO: Make a forgot password
+  //TODO: Make a route to the register
+  
+
 
 }
