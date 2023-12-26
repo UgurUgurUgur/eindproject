6c3da81b53fcee5d0dfa4c90e138fd2bf349fa36
liamvdm <L1am20041711@gmail.com>
2023-12-26 13:45:45 +0100

    added newpost function

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+
+  title = '';
+  image = '';
+  description = '';
+
+
+  newPost() {                                       /* this function is called when the new post button is pressed  */
+    console.log("title: " + this.title);
+    console.log( "image: " + this.image);
+    console.log( "description: " + this.description);
+
+    this.title = '';
+    this.image = '';     
+    this.description = '';
+  }
+
 
 }
