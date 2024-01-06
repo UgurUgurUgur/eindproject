bcb12498206bd99349d256d63730c5f84f39d7dc
ElineBoffinPXL <eline.boffin@student.pxl.be>
2024-01-06 14:47:20 +0100

    Contact page and nav style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,24 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent
-    }
-,
+    },
     {
         path: 'login',
         component: LoginComponent
-    }
-,
+    },
     {
         path: 'admin',
         component: AdminComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
     }
     
 ];
